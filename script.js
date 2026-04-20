31a3a44bb85c6656c7848986044361a628f7ba10
Tarun Yadav <taruny900@gmail.com>
2026-04-20 20:57:34 +0530

    Updated code (making attractive )

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('play-button');
+
+button.addEventListener('click', () => {
+    window.location.href = "https://taruny900-ui.github.io/Stone-Paper-scissors/";
+});
